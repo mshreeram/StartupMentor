7d299293d75b064d360114a08704245322e1f482
Shree Ram <mekashreeram@gmail.com>
2023-10-07 15:16:36 +0530

    Create Initial App

---
+++ startup/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(req):
+  return render(req, 'home.html')
+
+def registerAsMentor(req):
+  return render(req, 'mentor/register.html')
+
+def registerAsStartUp(req):
+  return render(req, 'startup/register.html')
+
+def registerAsStudent(req):
+  return render(req, 'student/register.html')
+
+def loginAsMentor(req):
+  return render(req, 'mentor/login.html')
